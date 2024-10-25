c587bdcd1069932fa1564d1894dca448b6c92266
nghiadeveloper <hieunghia.dev@gmail.com>
2024-10-25 22:22:10 +0700

    Bài tập lớn 04

--- baitaplon04/src/baitaplon04/BaiTap01.java
+++ baitaplon04/src/baitaplon04/BaiTap01.java
@@ -90,19 +90,19 @@ public class BaiTap01 {
 
 
     
-    /**
-     * c. Tìm số âm đầu tiên và vị trí của nó trong mảng
-     * 
-     * Theo mô hình 3 khối ta có:
+	/**
+	 * c. Tìm số âm đầu tiên và vị trí của nó trong mảng
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập vào cho mảng thực hiện tìm số âm đầu tiên và vị trí của nó trong mảng
 	 * - Đầu ra: Tìm số âm đầu tiên và vị trí của nó trong mảng
-	 * - Xử lý: 
+	 * - Xử lý:
 	 * 		+ Sử dụng vòng lặp for: Bắt đầu lặp từ phần tử đầu tiên (chỉ số 0) cho đến hết mảng. Vòng lặp này sẽ kiểm tra từng phần tử trong mảng
 	 * 		+ Kiểm tra xem phần tử hiện tại (array[i]) có phải là số âm hay không? bằng cách so sánh giá trị của phần tử với 0
 	 * 		+ In ra số âm đầu tiên tìm thấy và vị trí của nó trong mảng
 	 * 		+ Kết thúc phương thức ngay sau khi tìm thấy số âm đầu tiên (return)
-     */
-    
+	 */
+	
 	public static void findFirstNegative(int[] array) {
 	    for (int i = 0; i < array.length; i++) {
 	        if (array[i] < 0) {
@@ -115,28 +115,26 @@ public class BaiTap01 {
 	}
 
 
-
-    /**
-     * d. Tìm số âm lớn nhất và vị trí của nó trong mảng
-     * 
-     * Theo mô hình 3 khối ta có:
+	
+	/**
+	 * d. Tìm số âm lớn nhất và vị trí của nó trong mảng
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập vào cho mảng thực hiện tìm số âm lớn nhất và vị trí của nó trong mảng
 	 * - Đầu ra: Tìm số âm lớn nhất và vị trí của nó trong mảng
-	 * - Xử lý: 
+	 * - Xử lý:
 	 * 		+ Khai báo biến maxNeg với giá trị nhỏ nhất của kiểu số nguyên (Integer.MIN_VALUE)
 	 * 		+ Khai báo biến position với giá trị -1, dùng để lưu vị trí của số âm lớn nhất. Giá trị -1 cũng được sử dụng để kiểm tra xem có số âm nào trong mảng hay không?
 	 * 		+ Sử dụng vòng lặp fpr: lặp qua từng phần tử trong mảng từ chỉ số 0 đến chỉ số cuối cùng
-	 * 		+ Kiểm tra xem phần tử hiện tại (array[i]) có phải là số âm và lớn hơn giá trị maxNeg hay không? 
-	 * 		
+	 * 		+ Kiểm tra xem phần tử hiện tại (array[i]) có phải là số âm và lớn hơn giá trị maxNeg hay không?
 	 * 		+ Nếu thỏa điều kiện:
 	 * 			. Cập nhật giá trị của maxNeg thành phần tử âm hiện tại
 	 * 			. Cập nhật vị trí của số âm lớn nhất thành chỉ số của phần tử hiện tại
-	 * 
 	 * 		+ Kiểm tra xem có tìm thấy số âm nào không?
 	 * 			. Nếu position khác -1 (đã tìm thấy ít nhất một số âm)
 	 * 			. Nếu có số âm lớn nhất: In ra số âm lớn nhất (maxNeg) và vị trí của nó (position)
 	 * 			. Ngược lại không có số âm nào 
-     */
+	 */
     
 	public static void findMaxNegative(int[] array) {
 	    int maxNeg = Integer.MIN_VALUE;
@@ -157,21 +155,21 @@ public class BaiTap01 {
 	}
 
     
-    
-    /**
-     * e. Tính tổng các số chẵn
-     * 
-     * Theo mô hình 3 khối ta có:
+	
+	/**
+	 * e. Tính tổng các số chẵn
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập vào cho mảng thực hiện tính tổng các số chẵn
 	 * - Đầu ra: Tính tổng các số chẵn
-	 * - Xử lý: 
+	 * - Xử lý:
 	 * 		+ Khai báo biến sum với giá trị 0 dùng để lưu tổng của các số chẵn trong mảng
 	 * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
-	 * 		+ Kiểm tra xem số hiện tại (num) có phải là số chẵn hay không? Bằng cách sử dụng phép chia lấy dư %: 
-	 * 			. Nếu num chia cho 2 còn dư 0 thì số đó là số chẵn
+	 * 		+ Kiểm tra xem số hiện tại (num) có phải là số chẵn hay không? Bằng cách sử dụng phép chia lấy dư %:
+	 *			. Nếu num chia cho 2 còn dư 0 thì số đó là số chẵn
 	 * 			. Nếu thỏa điều kiện trên thì số chẵn hiện tại sẽ được cộng vào sum
 	 * 		+ Trả về sum
-     */
+	 */
     
 	public static int sumEven(int[] array) {
 	    int sum = 0;
@@ -186,20 +184,20 @@ public class BaiTap01 {
 	}
 
 
-
-    /**
-     * f. Đếm có bao nhiêu số âm
-     * 
-     * Theo mô hình 3 khối ta có:
+	
+	/**
+	 * f. Đếm có bao nhiêu số âm
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập vào cho mảng thực hiện đếm có bao nhiêu số âm
 	 * - Đầu ra: Đếm có bao nhiêu số âm
-	 * - Xử lý: 
+	 * - Xử lý:
 	 * 		+ Khai báo biến count với giá trị 0 dùng để lưu trữ số lượng số âm trong mảng
 	 * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
 	 * 		+ Kiểm tra xem số hiện tại (num) có phải là số âm hay không?
 	 * 			. Nếu thỏa điều kiện thì biến count sẽ được tăng lên 1 (count++)
 	 * 		+ Trả về count
-     */
+	 */
     
 	public static int countNegative(int[] array) {
 	    int count = 0;
@@ -214,20 +212,20 @@ public class BaiTap01 {
 	}
 
     
-
-    /**
-     * g. Tổng các số âm
-     * 
-     * Theo mô hình 3 khối ta có:
+	
+	/**
+	 * g. Tổng các số âm
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập vào cho mảng thực hiện tính tổng các số âm
 	 * - Đầu ra: Tính tổng các số âm
-	 * - Xử lý: 
-     * 		+ Khai báo biến sum với giá trị 0 dùng để lưu tổng của các số âm trong mảng
-     * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
-     * 		+ Kiểm tra xem số hiện tại (num) có phải là số âm hay không?
-     * 			. Nếu thỏa điều kiện trên, số âm hiện tại sẽ được cộng vào sum
-     * 		+ Trả về sum
-     */
+	 * - Xử lý:
+	 * 		+ Khai báo biến sum với giá trị 0 dùng để lưu tổng của các số âm trong mảng
+	 * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
+	 * 		+ Kiểm tra xem số hiện tại (num) có phải là số âm hay không?
+	 *  		. Nếu thỏa điều kiện trên, số âm hiện tại sẽ được cộng vào sum
+	 *  	+ Trả về sum
+	 */
     
 	public static int sumNegative(int[] array) {
 	    int sum = 0;
@@ -241,19 +239,19 @@ public class BaiTap01 {
 	    return sum;
 	}
 
-    
-    
-    /**
-     * h. Cho người dùng nhập vào x và tìm x có trong mảng đó hay không?
-     * 
-     * Theo mô hình 3 khối ta có:
+
+	
+	/**
+	 * h. Cho người dùng nhập vào x và tìm x có trong mảng đó hay không?
+	 * 
+	 * Theo mô hình 3 khối ta có:
 	 * - Đầu vào: Từ các giá trị mà người dùng nhập và yêu cầu người dùng nhập vào số nguyên x
 	 * - Đầu ra: Tìm số nguyên x mà người dùng nhập vào có tồn tại trong mảng đó hay không?
-	 * - Xử lý: 
-     * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
-     * 		+ Kiểm tra xem phần tử hiện tại (num) có bằng với giá trị x hay không?
-     * 		+ Nếu thỏa điều kiện trên: In ra thông báo rằng số x có trong mảng
-     */
+	 * - Xử lý:
+	 * 		+ Sử dụng vòng lặp foreach để duyệt qua từng phần tử trong mảng. Mỗi phần tử được gán cho biến num trong mỗi lần lặp
+	 * 		+ Kiểm tra xem phần tử hiện tại (num) có bằng với giá trị x hay không?
+	 * 		+ Nếu thỏa điều kiện trên: In ra thông báo rằng số x có trong mảng
+	 */
 	
 	public static void findXInArray(int[] array, int x) {
 	    for (int num : array) {
