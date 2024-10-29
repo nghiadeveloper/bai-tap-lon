6a48c4c6679997cce92cfad57c7b25dfb8adf12a
nghiadeveloper <hieunghia.dev@gmail.com>
2024-10-29 13:38:35 +0700

    Bài tập lớn 04

--- baitaplon04/src/baitaplon04/BaiTap08.java
+++ baitaplon04/src/baitaplon04/BaiTap08.java
@@ -14,11 +14,11 @@ public class BaiTap08 {
 		Scanner scanner = new Scanner(System.in);
         
 		System.out.print("Nhập vào số phần tử n: ");
-		    int n = scanner.nextInt();
-		   
-		    int[] numbers = new int[n];
-		    
-		    System.out.println("Nhập vào các phần tử của mảng:");
+		int n = scanner.nextInt();
+		
+		int[] numbers = new int[n];
+		
+		System.out.println("Nhập vào các phần tử của mảng:");
 		for (int i = 0; i < n; i++) {
 		    System.out.print("Phần tử thứ " + (i + 1) + ": ");
 		    numbers[i] = scanner.nextInt();
